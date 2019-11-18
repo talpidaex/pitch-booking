cb861b5499f45e48755c6543842b2d66e0f79918
talpidaex <bayrakogzhn@gmail.com>
2019-11-18 16:34:12 +0300

    admin giriş eklendi , üye giris ve duyurular eklendi

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 var app = express();
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 app.get("/", function(req, res) {
   res.render("anasayfa");
-})
+});
+
+app.get("/uye-giris", function(req, res) {
+  res.render("uyeGiris");
+});
+
+app.get("/admin-giris", function(req, res) {
+  res.render("admin-giris");
+});
+app.get("/iletisim", function(req, res) {
+  res.render("iletisim");
+});
+
+
 
 
 app.listen(3000, function() {
