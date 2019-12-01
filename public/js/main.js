b25a5a1a0ee0c58b6e565ce119b420ea6a6267b9
talpidaex <bayrakogzhn@gmail.com>
2019-12-01 14:43:58 +0300

    navbar'a dropdown eklendi , tüm sayfalardaki css'ler kontrol edildi ve düzeltildi , buttonlara alert mesajlarını eklendi, admin-giriş buttonuna gecici olarak get istediğyle admin-Dashboard'a yönlendirildi

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+// Oğuzhan Bayrak JS
+
+//Gecici olarak oluşturulan Alert mesajları- Back-end sonrası Düzeltilecektir!
+
+$("#uye-giris-button").click(function() {
+  alert("Uye Giriş Başarılı!");
+});
+$("#admin-giris-button").click(function() {
+  alert("Admin Giriş Başarılı!");
+});
+$("#admin-iletisim-güncelle-button").click(function() {
+  alert("İletişim Bilgileri Başarıyla Güncellendi!");
+});
+$("#admin-sifre-degistir").click(function() {
+  alert("Şifre Başarıyla Değiştirildi!");
+});
+$("#admin-duyuru-button").click(function() {
+  alert("Duyuru Eklendi!");
+});
+$("#admin-image-add-button").click(function() {
+  alert("Yeni Fotoğraflar Eklendi!");
+});
