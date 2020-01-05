8e8cc81535a067482547a79d601aa36f7cc70c97
talpidaex <bayrakogzhn@gmail.com>
2020-01-05 18:49:14 +0300

    .gitignore,procfile ve port değiştirildi heroku için gerekli adımlar

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ app.use(apiRoutes);
 //metecapar
 
 //--------------Server-------------------
+let port = process.env.PORT;
+if (port == null || port = "") {
+  port = 3000;
+}
 app.listen(3000, function() {
   console.log("Dinleninen port : 3000");
 })
